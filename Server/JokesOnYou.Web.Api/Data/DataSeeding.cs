56e7c939458d6b55cce9ffdb8fd8fa805b37bb7e
Nico <niccolo.caiti@gmail.com>
2021-03-19 15:30:24 +0100

    Created DataSeeding Class

---
+++ Server/JokesOnYou.Web.Api/Data/DataSeeding.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Data
+{
+    public class DataSeeding
+    {
+
+    }
+}
