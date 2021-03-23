418d9dbd7a3a9d011965a8e7b0c6e60fd18b7e40
chris <christianhunter707@gmail.com>
2021-03-23 22:18:58 +0100

    interface for user repo

---
+++ Server/JokesOnYou.Web.Api/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JokesOnYou.Web.Api.Models;
+
+namespace JokesOnYou.Web.Api.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(int id);
+        Task<IEnumerable<User>> GetUsers();
+        Task DeleteUser(int id);
+        void UpdateUser();
+    }
+}
