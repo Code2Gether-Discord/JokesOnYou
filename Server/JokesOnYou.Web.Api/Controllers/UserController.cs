7ec497135da39f58e93cbf3274f143182c46fce8
Max <hellhunter15@hotmail.com>
2021-03-24 05:30:56 +0100

    added UserController and UserService Skeleton

---
+++ Server/JokesOnYou.Web.Api/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using JokesOnYou.Web.Api.Controllers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Controllers
+{
+    public class UserController : IUserController
+    {
+    }
+}
