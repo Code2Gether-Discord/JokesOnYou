ebad8b8a4e76b0ea70aebdb4d13cb7b89dc3a343
chris <christianhunter707@gmail.com>
2021-04-06 10:10:57 +0200

    removed explicit base keyword

--- Server/JokesOnYou.Web.Api/Exceptions/AppException.cs
+++ Server/JokesOnYou.Web.Api/Exceptions/AppException.cs
@@ -4,7 +4,7 @@ namespace JokesOnYou.Web.Api.Exceptions
 {
     public class AppException : Exception
     {
-        public AppException() : base()
+        public AppException()
         {
         }
 
