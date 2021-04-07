1179f50d429b3a7c527dda3737a03968a498e25f
chris <christianhunter707@gmail.com>
2021-04-07 15:09:21 +0200

    Updated repository

--- Server/JokesOnYou.Web.Api/Repositories/UserRepository.cs
+++ Server/JokesOnYou.Web.Api/Repositories/UserRepository.cs
@@ -39,29 +39,12 @@ namespace JokesOnYou.Web.Api.Repositories
             }
         }
 
-        public async Task DeleteUserAsync(User user)
-        {
-            await _userManager.DeleteAsync(user);
-        }
+        public Task DeleteUserAsync(User user) => _userManager.DeleteAsync(user);
 
-        public Task<User> GetUserAsync(string id)
-        {
-            //Possibilities 
-            //var user = await _userManager.FindByIdAsync(id);
-            //var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
-            return _userManager.FindByIdAsync(id);
-        }
+        public Task<User> GetUserAsync(string id) => _userManager.FindByIdAsync(id);
 
-        public Task<User> GetUserByEmail(string email)
-        {
-            return _userManager.FindByEmailAsync(email);
-        }
+        public Task<User> GetUserByEmail(string email) => _userManager.FindByEmailAsync(email);
 
-        public async Task<IEnumerable<User>> GetUsersAsync()
-        {
-            //Does not need to be awaited here.
-            var users = await _userManager.Users.ToListAsync();
-            return users;
-        }
+        public async Task<IEnumerable<User>> GetUsersAsync() => await _userManager.Users.ToListAsync();
     }
 }
