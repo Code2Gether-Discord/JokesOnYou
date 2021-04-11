fab44ae751710089eff3f18f808ad4ac0c4f3c4c
Max <hellhunter15@hotmail.com>
2021-04-11 06:27:35 +0200

    Created Tag Skeleton;
    Added Controller, Service, repo, Model and Dto's as empty classes and interfaces.

---
+++ Server/JokesOnYou.Web.Api/Controllers/TagController.cs
@@ -0,0 +1,18 @@
+﻿using JokesOnYou.Web.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Controllers
+{
+    public class TagController
+    {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+    }
+}
