9fd67a4e82fddeacb7c09ea204aee81a18d4136a
Max <hellhunter15@hotmail.com>
2021-04-11 06:19:07 +0200

    Added Joke Skeleton;
    Added Controller, Service, Repo and Dto's empty classes and interfaces to be worked on.

---
+++ Server/JokesOnYou.Web.Api/Controllers/JokesController.cs
@@ -0,0 +1,19 @@
+﻿using JokesOnYou.Web.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Controllers
+{
+    public class JokesController : ControllerBase
+    {
+        private readonly IJokesService _jokesService;
+
+        public JokesController(IJokesService jokesService)
+        {
+            _jokesService = jokesService;
+        }
+    }
+}
---
+++ Server/JokesOnYou.Web.Api/Services/JokesService.cs
@@ -0,0 +1,19 @@
+﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+using JokesOnYou.Web.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Services
+{
+    public class JokesService : IJokesService
+    {
+        private readonly IJokesRepository _jokesRepo;
+
+        public JokesService(IJokesRepository jokesRepo)
+        {
+            _jokesRepo = jokesRepo;
+        }
+    }
+}
