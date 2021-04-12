5722f0f4a603f853685d4af0ca3ee9b1a8c23418
Max <hellhunter15@hotmail.com>
2021-04-12 04:51:47 +0200

    Created empty LikedJoke skeleton.
    Added Controller, Service, Repository, Interfaces, Model and Dto's.

---
+++ Server/JokesOnYou.Web.Api/Services/LikedJokeService.cs
@@ -0,0 +1,18 @@
+﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Services
+{
+    public class LikedJokeService
+    {
+        private readonly ILikedJokeRepository _likedJokeRepo;
+
+        public LikedJokeService(ILikedJokeRepository likedJokeRepo)
+        {
+            _likedJokeRepo = likedJokeRepo;
+        }
+    }
+}
