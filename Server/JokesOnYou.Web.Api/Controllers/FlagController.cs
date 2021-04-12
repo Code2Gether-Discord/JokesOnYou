1f4cee14635d010b649274fc76d328a75e19ff9c
Max <hellhunter15@hotmail.com>
2021-04-12 04:34:26 +0200

    Created empty Flag Skeleton and models.
    Added Controller, Service, Repository, Interfaces, Model and Dto's.

---
+++ Server/JokesOnYou.Web.Api/Controllers/FlagController.cs
@@ -0,0 +1,18 @@
+﻿using JokesOnYou.Web.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Controllers
+{
+    public class FlagController
+    {
+        private readonly IFlagService _flagService;
+
+        public FlagController(IFlagService flagService)
+        {
+            _flagService = flagService;
+        }
+    }
+}
