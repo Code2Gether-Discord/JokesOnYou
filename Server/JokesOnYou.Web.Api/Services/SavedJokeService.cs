baeef72f474032ea276d5859eeba0c28e1bde9e2
Max <hellhunter15@hotmail.com>
2021-04-12 04:57:37 +0200

    Created empty SavedJoke Skeleton.
    Added Controller, Service, Repository, Interfaces, Model and Dto's.

---
+++ Server/JokesOnYou.Web.Api/Services/SavedJokeService.cs
@@ -0,0 +1,18 @@
+﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Services
+{
+    public class SavedJokeService
+    {
+        private readonly ISavedJokeRepository _savedJokeRepo;
+
+        public SavedJokeService(ISavedJokeRepository savedJokeRepo)
+        {
+            _savedJokeRepo = savedJokeRepo;
+        }
+    }
+}
