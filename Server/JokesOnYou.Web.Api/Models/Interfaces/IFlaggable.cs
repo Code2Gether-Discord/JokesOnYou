022c76e327f042389bb54de8d8ddda1a0749de73
Max <hellhunter15@hotmail.com>
2021-04-13 04:52:17 +0200

    Moved IFlaggable into Interfaces folder.

--- Server/JokesOnYou.Web.Api/Models/Interfaces/IFlaggable.cs
+++ Server/JokesOnYou.Web.Api/Models/Interfaces/IFlaggable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace JokesOnYou.Web.Api.Models
+namespace JokesOnYou.Web.Api.Models.Interfaces
 {
     public interface IFlaggable
     {
