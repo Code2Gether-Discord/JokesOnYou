bf28bca3e5c838222906c88e29aec37bc98214b7
Max <hellhunter15@hotmail.com>
2021-04-13 04:41:19 +0200

    added IFlaggable.
    This way we will be able to flag multiple Models with the same flags.
    Need to add it to User, Joke and Tag.

---
+++ Server/JokesOnYou.Web.Api/Models/IFlaggable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.Models
+{
+    public interface IFlaggable
+    {
+        public List<Flag> CurrentFlags { get; set; }
+        public int TimesFlagged { get; set; }
+    }
+}
