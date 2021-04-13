b1df8f74d45e58b8a459c55a2e403fbbd2c5b25a
Max <hellhunter15@hotmail.com>
2021-04-13 04:54:24 +0200

    Added Tag props.

--- Server/JokesOnYou.Web.Api/Models/Tag.cs
+++ Server/JokesOnYou.Web.Api/Models/Tag.cs
@@ -7,5 +7,10 @@ namespace JokesOnYou.Web.Api.Models
 {
     public class Tag
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public User Owner { get; set; }
+        public int Likes { get; set; }
     }
 }
