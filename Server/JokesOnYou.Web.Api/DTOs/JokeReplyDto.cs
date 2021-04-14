722775b545821b84f965cfaf99cebf58403ca203
Max <hellhunter15@hotmail.com>
2021-04-14 03:10:27 +0200

    Added JokesReplyDto props

--- Server/JokesOnYou.Web.Api/DTOs/JokeReplyDto.cs
+++ Server/JokesOnYou.Web.Api/DTOs/JokeReplyDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JokesOnYou.Web.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace JokesOnYou.Web.Api.DTOs
 {
     public class JokeReplyDto
     {
+        public int Id { get; set; }
+        public string Premise { get; set; }
+        public string Punchline { get; set; }
+        public User Author { get; set; }
+        public DateTime UploadDate { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
     }
 }
