914445335123664c20a2799c78dc2e1646e0a5d4
Max <hellhunter15@hotmail.com>
2021-04-14 04:54:52 +0200

    added Controller GetAllJokesAsync method.
    Gets the joke Dto's from the Service and returns Ok with the Dto's.

--- Server/JokesOnYou.Web.Api/Controllers/JokesController.cs
+++ Server/JokesOnYou.Web.Api/Controllers/JokesController.cs
@@ -1,4 +1,6 @@
-﻿using JokesOnYou.Web.Api.Services.Interfaces;
+﻿using JokesOnYou.Web.Api.DTOs;
+using JokesOnYou.Web.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace JokesOnYou.Web.Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class JokesController : ControllerBase
     {
         private readonly IJokesService _jokesService;
@@ -15,5 +20,14 @@ namespace JokesOnYou.Web.Api.Controllers
         {
             _jokesService = jokesService;
         }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JokeReplyDto>>> GetAllJokesAsync()
+        {
+            var jokeDtos = await _jokesService.GetAllJokesAsync();
+            return Ok(jokeDtos);
+        }
+
     }
 }
