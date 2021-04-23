ad74de641924eb9ec3b4d44e0e5533c6cd163f01
Max <hellhunter15@hotmail.com>
2021-04-23 04:48:08 +0200

    updated database with AuthorId

---
+++ Server/JokesOnYou.Web.Api/Migrations/20210423023914_ChangedModelIntoId.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesOnYou.Web.Api.Migrations
+{
+    public partial class ChangedModelIntoId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jokes_AspNetUsers_AuthorId",
+                table: "Jokes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jokes_AuthorId",
+                table: "Jokes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Jokes_AuthorId",
+                table: "Jokes",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jokes_AspNetUsers_AuthorId",
+                table: "Jokes",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
