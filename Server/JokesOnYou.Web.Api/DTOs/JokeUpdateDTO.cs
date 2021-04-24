0dd07fe78da417352ba3662eb55d9dd38143da29
oliver fu <ofu997@gmail.com>
2021-04-24 02:17:57 -0700

    add jokeupdateDTO

---
+++ Server/JokesOnYou.Web.Api/DTOs/JokeUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesOnYou.Web.Api.DTOs
+{
+    // Does not update joke's id, likes, author, uploaddate, or dislikes. Just Premise and Punchline
+    public class JokeUpdateDTO
+    {
+        public string Premise { get; set; }
+        public string Punchline { get; set; }
+    }
+}
