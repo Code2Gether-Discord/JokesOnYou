2ba7535e5b0dc2e9620935112193302cf68eb8d4
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2021-04-29 02:35:00 +0500

    migrate tag table to db

--- Server/JokesOnYou.Web.Api/Migrations/20210428213128_addTag.Designer.cs
+++ Server/JokesOnYou.Web.Api/Migrations/20210428213128_addTag.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using JokesOnYou.Web.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokesOnYou.Web.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210428213128_addTag")]
+    partial class addTag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
