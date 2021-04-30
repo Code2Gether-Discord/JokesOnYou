27f949860383f02ee50cc3f6d29c780669f9c612
Oguzhan <aksoy.ouzhan@ciel.ro>
2021-04-30 20:25:24 +0300

    DeleteTag related methods added

--- Server/JokesOnYou.Web.Api/Repositories/TagRepository.cs
+++ Server/JokesOnYou.Web.Api/Repositories/TagRepository.cs
@@ -1,4 +1,8 @@
-﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+﻿using AutoMapper;
+using JokesOnYou.Web.Api.Data;
+using JokesOnYou.Web.Api.DTOs;
+using JokesOnYou.Web.Api.Models;
+using JokesOnYou.Web.Api.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +10,64 @@ using System.Threading.Tasks;
 
 namespace JokesOnYou.Web.Api.Repositories
 {
-    public class TagRepository : ITagRepository
+    public class TagRepository : UnitOfWork,ITagRepository
     {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public TagRepository(DataContext context,IMapper mapper):base(context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Deletes given <paramref name="tag"/>
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public async Task<bool> Delete(TagCreateDto tag)
+        {
+            var entity = _mapper.Map<Tag>(tag);
+            _context.Remove(entity);
+            return await SaveAsync();
+        }
+
+
+        /// <summary>
+        /// Deletes given <paramref name="tagId"/>
+        /// </summary>
+        /// <param name="tagId"></param>
+        /// <returns></returns>
+        public async Task<bool> Delete(int tagId)
+        {
+            var entity = _context.Tags.Find(tagId);
+            _context.Remove(entity);
+            return await SaveAsync();
+        }
+
+
+        /// <summary>
+        /// Deletes all given <paramref name="tags"/>
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteRange(List<TagCreateDto> tags)
+        {
+            var entity = _mapper.Map<List<Tag>>(tags);
+            _context.RemoveRange(entity);
+            return await SaveAsync();
+        }
+
+
+        /// <summary>
+        /// Deletes all given <paramref name="tags"/>
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteRange(int[] tags)
+        {
+            var entities = _context.Tags.Where(x=>tags.Contains(x.Id)).ToList();
+            _context.RemoveRange(entities);
+            return await SaveAsync();
+        }
     }
 }
--- Server/JokesOnYou.Web.Api/Services/TagService.cs
+++ Server/JokesOnYou.Web.Api/Services/TagService.cs
@@ -1,4 +1,5 @@
-﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+﻿using JokesOnYou.Web.Api.DTOs;
+using JokesOnYou.Web.Api.Repositories.Interfaces;
 using JokesOnYou.Web.Api.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,47 @@ namespace JokesOnYou.Web.Api.Services
         public TagService(ITagRepository tagRepo)
         {
             _tagRepo = tagRepo;
+        }
+        /// <summary>
+        /// Deletes given <paramref name="tag"/>
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteTag(TagCreateDto tag)
+        {
+            return await _tagRepo.Delete(tag);
+        }
+
+        /// <summary>
+        /// Deletes given <paramref name="tagId"/>
+        /// </summary>
+        /// <param name="tagId"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteTag(int tagId)
+        {
+            return await _tagRepo.Delete(tagId);
+        }
+
+
+        /// <summary>
+        /// Deletes all given <paramref name="tags"/>
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteMultipleTag(List<TagCreateDto> tags)
+        {
+            return await _tagRepo.DeleteRange(tags);
+        }
+
+
+        /// <summary>
+        /// Deletes all given <paramref name="tagIds"/>
+        /// </summary>
+        /// <param name="tagIds"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteMultipleTag(int[] tagIds)
+        {
+            return await _tagRepo.DeleteRange(tagIds);
         }
     }
 }
