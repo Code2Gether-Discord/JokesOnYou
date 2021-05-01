c516e0406170c4fd86e65750fa543cc5756f41b2
Max <hellhunter15@hotmail.com>
2021-05-01 05:15:55 +0200

    initialized lists and changed to ICollection<int>.

--- Server/JokesOnYou.Web.Api/Models/LikedTags.cs
+++ Server/JokesOnYou.Web.Api/Models/LikedTags.cs
@@ -10,7 +10,7 @@ namespace JokesOnYou.Web.Api.Models
         public int Id { get; set; }
         public int JokeId { get; set; }
         public DateTime SavedDate { get; set; } = DateTime.Now;
-        public List<int> Tags { get; set; }
+        public ICollection<int> Tags { get; set; } = new List<int>();
         public string UserId { get; set; }
     }
 }
