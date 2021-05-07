bf57625736a3e59db3b98e3062ec33f348d66fa8
oliver fu <ofu997@gmail.com>
2021-05-06 23:44:47 -0700

    add TagController

--- Server/JokesOnYou.Web.Api/Controllers/TagController.cs
+++ Server/JokesOnYou.Web.Api/Controllers/TagController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JokesOnYou.Web.Api.DTOs;
 
 namespace JokesOnYou.Web.Api.Controllers
 {
@@ -14,6 +15,14 @@ namespace JokesOnYou.Web.Api.Controllers
         public TagController(ITagService tagService)
         {
             _tagService = tagService;
+        }
+
+        // uncomment this when testing [Authorize(Roles = "Registered,Admin")]
+        [HttpPost]
+        public async Task<ActionResult<TagReplyDto>> CreateTagAsync(TagCreateDto tagCreateDto)
+        {
+            var tagReplyDto = await _tagService.CreateTagAsync(tagCreateDto); 
+            return tagReplyDto;
         }
     }
 }
