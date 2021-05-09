9952ca9c789618a82c7d1dd05c581fca18108afe
oliver fu <ofu997@gmail.com>
2021-05-09 01:29:57 -0700

    add tag repo

--- Server/JokesOnYou.Web.Api/Repositories/Interfaces/ITagRepository.cs
+++ Server/JokesOnYou.Web.Api/Repositories/Interfaces/ITagRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using System;//unnecessary?
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; //unnecessary?
 using System.Threading.Tasks;
+using JokesOnYou.Web.Api.DTOs;
+using JokesOnYou.Web.Api.Models;
+
 
 namespace JokesOnYou.Web.Api.Repositories.Interfaces
 {
     public interface ITagRepository
     {
+        Task CreateTagAsync(Tag tag); 
     }
 }
--- Server/JokesOnYou.Web.Api/Repositories/TagRepository.cs
+++ Server/JokesOnYou.Web.Api/Repositories/TagRepository.cs
@@ -1,4 +1,7 @@
-﻿using JokesOnYou.Web.Api.Repositories.Interfaces;
+﻿using AutoMapper;
+using JokesOnYou.Web.Api.Data;
+using JokesOnYou.Web.Api.Models;
+using JokesOnYou.Web.Api.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +9,29 @@ using System.Threading.Tasks;
 
 namespace JokesOnYou.Web.Api.Repositories
 {
+
+
     public class TagRepository : ITagRepository
     {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public TagRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task CreateTagAsync(Tag tag)
+        {
+            try
+            {
+                await _context.Tags.AddAsync(tag).AsTask();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("inner exception is: {0}", e.InnerException.Message);
+            }
+        }
     }
 }
