f4860cb1909531f2470b64fd8b03ea48ac53c81e
oliver fu <ofu997@gmail.com>
2021-05-09 01:29:25 -0700

    add tag service

--- Server/JokesOnYou.Web.Api/Services/Interfaces/ITagService.cs
+++ Server/JokesOnYou.Web.Api/Services/Interfaces/ITagService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JokesOnYou.Web.Api.DTOs;
 
 namespace JokesOnYou.Web.Api.Services.Interfaces
 {
     public interface ITagService
     {
+        Task<TagReplyDto> CreateTagAsync(TagCreateDto tagCreateDto);
     }
 }
--- Server/JokesOnYou.Web.Api/Services/TagService.cs
+++ Server/JokesOnYou.Web.Api/Services/TagService.cs
@@ -4,16 +4,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JokesOnYou.Web.Api.DTOs;
+using JokesOnYou.Web.Api.Exceptions;
+using AutoMapper;
+using JokesOnYou.Web.Api.Models;
 
 namespace JokesOnYou.Web.Api.Services
 {
     public class TagService : ITagService
     {
         private readonly ITagRepository _tagRepo;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public TagService(ITagRepository tagRepo)
+        public TagService(ITagRepository tagRepo, IMapper mapper, IUnitOfWork unitOfWork)
         {
             _tagRepo = tagRepo;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<TagReplyDto> CreateTagAsync(TagCreateDto tagCreateDto)
+        {
+            var tag = _mapper.Map<Tag>(tagCreateDto);
+            await _tagRepo.CreateTagAsync(tag);
+
+            var saved = await _unitOfWork.SaveAsync();
+            if (!saved)
+            {
+                throw new AppException("error saving tag"); 
+            }
+
+            var tagReplyDto = _mapper.Map<TagReplyDto>(tag);
+
+            return tagReplyDto;
         }
     }
 }
