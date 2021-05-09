e3df572e8359d2220c6f110938b00fa371ac6bcc
Nico <niccolo.caiti@gmail.com>
2021-05-09 17:30:22 +0200

    Added some fields to the UserUpdateDTO

--- Server/JokesOnYou.Web.Api/DTOs/UserUpdateDTO.cs
+++ Server/JokesOnYou.Web.Api/DTOs/UserUpdateDTO.cs
@@ -7,6 +7,9 @@ namespace JokesOnYou.Web.Api.DTOs
 {
     public class UserUpdateDTO
     {
+        public string Name { get; set; } //Make sure that name is unique
+        public string Role { get; set; }
+        public bool Nsfw { get; set; } //Unsure if this means that the user posted nsfw or just that the user wants to see nsfw
 
     }
 }
