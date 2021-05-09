87b85866044b3c87dddcf0782bdc02dbfd0b6507
oliver fu <ofu997@gmail.com>
2021-05-09 01:14:01 -0700

    add Tag model

--- Server/JokesOnYou.Web.Api/Models/Tag.cs
+++ Server/JokesOnYou.Web.Api/Models/Tag.cs
@@ -10,7 +10,7 @@ namespace JokesOnYou.Web.Api.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Created { get; set;  } = DateTime.Now;
-        public ICollection<int> Jokes { get; set; } = new List<int>();
+      //  public ICollection<int> Jokes { get; set; } = new List<int>();
         public string OwnerId { get; set; }
         public int Likes { get; set; }
     }
