d667655b10526b22c40d4a1c3324497dd58e3a62
oliver fu <ofu997@gmail.com>
2021-05-16 23:51:48 -0700

    simplify TagCreateDto

--- Server/JokesOnYou.Web.Api/DTOs/TagCreateDto.cs
+++ Server/JokesOnYou.Web.Api/DTOs/TagCreateDto.cs
@@ -7,9 +7,7 @@ namespace JokesOnYou.Web.Api.DTOs
 {
     public class TagCreateDto
     {
-        // public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
         public int Likes { get; set; }
     }
 }
