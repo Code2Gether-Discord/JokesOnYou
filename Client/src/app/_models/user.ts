63e5a80b35c7e28c566720e79e1210e4a4201205
chrisk <christian.khalil200@gmail.com>
2021-05-20 21:02:29 +0200

    account service and user model

--- Client/src/app/_models/user.ts
+++ Client/src/app/_models/user.ts
@@ -1,7 +1,7 @@
 export interface User {
-    id: string,
-    email: string,
-    userName: string,
-    role: string,
-    token: string
+    id?: string;
+    email?: string;
+    userName?: string;
+    role?: string;
+    token?: string;
 }
