7cf2ff50035fb05c4f1b27a58bfdff765a3db42d
Stuart Wessels <stustuwessels@hotmail.co.uk>
2021-05-20 21:37:12 +0100

    adding missing merge line

--- Server/JokesOnYou.Web.Api/Controllers/UserController.cs
+++ Server/JokesOnYou.Web.Api/Controllers/UserController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System;
 using System.Threading.Tasks;
 
 namespace JokesOnYou.Web.Api.Controllers
