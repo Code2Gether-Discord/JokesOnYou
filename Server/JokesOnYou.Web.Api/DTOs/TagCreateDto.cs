af7095bcf100df4bbd88173ec37f7f481ffe8c7e
oliver fu <ofu997@gmail.com>
2021-05-20 22:55:49 -0700

    require a string value for tags

--- Server/JokesOnYou.Web.Api/DTOs/TagCreateDto.cs
+++ Server/JokesOnYou.Web.Api/DTOs/TagCreateDto.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace JokesOnYou.Web.Api.DTOs
 {
     public class TagCreateDto
     {
+        [Required()]
         public string Name { get; set; }
     }
 }
