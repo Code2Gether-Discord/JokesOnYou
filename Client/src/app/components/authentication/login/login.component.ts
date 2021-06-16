64a3e8b29c1e87699e772853f73519de36ba8c61
unbekanntunity <nguyen3lihn@gmail.com>
2021-06-16 20:03:10 +0200

    Add dark and light mode

--- Client/src/app/components/authentication/login/login.component.ts
+++ Client/src/app/components/authentication/login/login.component.ts
@@ -11,7 +11,15 @@ export class LoginComponent implements OnInit {
   otherServicesWidth: number = 219;
   otherServicesHeight: number = 37;
 
-  constructor() { }
+  darkMode: boolean = true;
+  bgColor: string;
+  fgColor: string;
+
+
+  constructor() {
+    this.bgColor = (this.darkMode) ? "#2C2F33" : "white";
+    this.fgColor = (this.darkMode) ? "white" : "black";
+  }
 
   ngOnInit(): void {
   }
--- Client/src/app/components/authentication/registration/registration.component.ts
+++ Client/src/app/components/authentication/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  darkMode: boolean = false;
+  bgColor: string;
+  fgColor: string;
 
-  constructor() { }
+  sliderClass: string;
+
+  constructor() {
+    this.bgColor = (this.darkMode) ? "#2C2F33" : "white";
+    this.fgColor = (this.darkMode) ? "white" : "black";
+    this.sliderClass = (this.darkMode) ? "sliderDark round" : "slider round";
+  }
 
   ngOnInit(): void {
   }
