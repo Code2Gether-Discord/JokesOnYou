6954eed2f5ac27aecf8f2eeed24c45faa79bbe76
Bormotov <bormotovk@gmail.com>
2021-10-04 17:18:02 +0300

    Default value for NSFW and go to jokes on logo click

--- Server/JokesOnYou.Web.Api/Models/Request/UserRegisterDTO.cs
+++ Server/JokesOnYou.Web.Api/Models/Request/UserRegisterDTO.cs
@@ -8,13 +8,16 @@ namespace JokesOnYou.Web.Api.Models.Request
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         [MinLength(4)]
         public string UserName { get; set; }
+
         [Required]
         public string Password { get; set; }
-        [Required]
-        public bool NsfwEnabled { get; set; }
+
+        public bool NsfwEnabled { get; set; } = false;
+
         [Required]
         public DateTime BirthDate { get; set; }
     }
