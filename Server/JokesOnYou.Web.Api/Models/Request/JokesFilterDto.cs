e2fbf6dcd3b5dce1c98253bd9e5000a4b2a4072f
TalatTanbir <hossaintanbir0@gmail.com>
2021-10-25 00:30:42 +0600

    Changed UserParams to PaginationParams

--- Server/JokesOnYou.Web.Api/Models/Request/JokesFilterDto.cs
+++ Server/JokesOnYou.Web.Api/Models/Request/JokesFilterDto.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace JokesOnYou.Web.Api.Models.Request
 {
-    public class JokesFilterDto : UserParameters
+    public class JokesFilterDto : PaginationParameters
     {
         public string AuthorId { get; set; }
         public int MinimumLikes { get; set; } = int.MinValue;
         public int MaximumLikes { get; set; } = int.MaxValue;
         public DateTime MinimumDate { get; set; } = DateTime.MinValue;
         public DateTime MaximumDate { get; set; } = DateTime.MaxValue;
+        public string SearchText { get; set; } = string.Empty;
     }
 }
