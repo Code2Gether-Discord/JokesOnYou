bc8f71c3b1fc5fe4e8e068a4acd4c962863e1d96
Max <hellhunter15@hotmail.com>
2021-11-18 05:55:32 +0100

    Add: Rules for Tags.
    - Add: StringExtention to make First Character To Upper and the Rest To Lower.
    - Tag will now be checked for whitespaces (not allowed)
    - Tag will be checked for any Characters that are not Letters.
    - Tag will be converted to FirstCharToUpperRestToLower.

--- Server/JokesOnYou.Web.Api/Models/Tag.cs
+++ Server/JokesOnYou.Web.Api/Models/Tag.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JokesOnYou.Web.Api.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -9,7 +10,29 @@ namespace JokesOnYou.Web.Api.Models
     public class Tag
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        private string name;
+        public string Name 
+        { 
+            get { return name; } 
+            set
+            {
+                VerifyTag(value);
+                name = value.FirstCharToUpperRestToLower();
+            }
+        }
+
+        public static void VerifyTag(string name)
+        {
+            if (name.Any(c=> Char.IsWhiteSpace(c)))
+            {
+                throw new ArgumentException($"Error: Tag Given is not a valid Tag. \"{name}\". Tag should not Contain any special characters, numbers or spaces.");
+            }
+            if (name.Any(ch => !Char.IsLetter(ch)))
+            {
+                throw new ArgumentException($"Error: Tag Given is not a valid Tag. \"{name}\". Tag should not Contain any special characters, numbers or spaces.");
+            }
+        }
+
         public DateTime Created { get; set; } = DateTime.UtcNow;
         [NotMapped]
         public ICollection<int> Jokes { get; set; } = new List<int>();
